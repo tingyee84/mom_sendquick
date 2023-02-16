6d120317e41ee38283b14a0005eb3d700a142b87
edwinsendquick <edwinng@sendquick.com>
2023-02-16 09:59:09 +0800

    scheduled_sms

--- mom/scheduled_sms.php
+++ mom/scheduled_sms.php
@@ -48,43 +48,6 @@
 	</div>
 	<script src="js/moment.min.js"></script>
 	<script src="js/datetime-moment.js"></script>
-	<script nonce="<?php echo session_id();?>">
-	$.fn.dataTable.moment('DD/MM/YYYY HH:mm:ss');
-	var table =  $('#schedule').DataTable({
-		autoWidth: false,
-		deferRender: true,
-		processing: true,
-		stateSave: true,
-		ajax: {type:'POST',url:'scheduled_sms_lib.php',data:{mode:'listScheduled'}},
-		columnDefs: [{ "orderable":false,"targets":5},
-					 { "width":"50%","targets":1}]
-	});
-	$('#all').change(function () {
-		var cells = table.cells().nodes();
-		$(cells).find(':checkbox').prop('checked',$(this).is(':checked'));
-	});
-	$('#delete').on('click', function(e) {
-		if(confirm('<?php echo $x->alert_2; ?>')) {
-			$('input[type=checkbox]').each(function() {     
-				if (this.checked && this.value!='on') {
-					$.post('scheduled_sms_lib.php',{mode:'deleteScheduled',idx:this.value},function(data) {
-						
-						//alert(data);
-						//return false;
-						table.ajax.reload();
-					});
-				}
-			});
-			$('#all').prop('checked',false);
-		}
-	});
-	$('#truncate').on('click', function(e) {
-		if(confirm('<?php echo $x->alert_3; ?>')) {
-			$.post('scheduled_sms_lib.php',{mode:'emptyScheduled'},function(data) {
-				table.ajax.reload();
-			});
-		}
-	});
-	</script>
+	<script type="application/javascript" src="scheduled_sms_js.php"></script>
 </body>
 </html>
