5a3ed5e2875292bf03961beffc6f9d401462f2e8
Ty Chang <tingyee@sendquick.com>
2023-02-17 09:49:41 +0800

    temp place dummy file for speed up loading

--- mom/js/vfs_fonts.js.php
+++ mom/js/vfs_fonts.js.php
@@ -1,6 +1,6 @@
 <?php
-header("Cache-Control: private,  max-age=1800");
 header("Content-type: text/javascript");
+header("Cache-Control: public,  max-age=86400");
 $my_file = fopen("vfs_fonts.js", "r"); 
 echo fgets($my_file);
 ?>
